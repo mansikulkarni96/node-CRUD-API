77f121d869cf521e389b259e82a819c92ec7f0af
Mansi <mansii.kulkarni29@gmail.com>
2019-08-17 04:19:18 -0400

    uncommented object

--- services/table.service.server.js
+++ services/table.service.server.js
@@ -188,7 +188,7 @@ module.exports = function (app,url,useDb) {
   			var keys = Object.keys(req.query);
   			var values = Object.values(req.query);
   			var ret = '';
-  			//var queryObj = {};
+  			var queryObj = {};
   			var operatorIndex = {'>':'$gt','>=':'$gte','<':'$lt','<=':'$lte','!=':'$ne'};
   			for(var i=0;i<keys.length;i++)
   			{
